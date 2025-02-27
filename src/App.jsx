9844706528e2908153b16fc2da6ce4e4674a080d
Azlan <azlanehtesham02@gmail.com>
2025-02-27 10:02:37 +0530

    logo update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import LandingPage from "./pages/LandingPage.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
 
 const App = () => {
   return (
     <>
       <LandingPage />
+      {/* <LoginPage /> */}
     </>
   );
 };
---
+++ src/components/Signup.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router";
+
+const Signup = () => {
+  return (
+    <>
+      <div></div>
+    </>
+  );
+};
+
+export default Signup;
