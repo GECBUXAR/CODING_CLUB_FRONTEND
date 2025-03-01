99fff4c3be1a8f38eda65e04d4f3b5b422636a16
Azlan <azlanehtesham02@gmail.com>
2025-03-01 22:25:02 +0530

    Signup page oriantation fix

--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -4,9 +4,14 @@ import Signup from "../components/Signup";
 const SignupPage = () => {
   return (
     <>
-      <div>
-        <Signup />
+      <div className="flex min-h-svh flex-col items-center justify-center bg-muted ">
+        <div className="w-full max-w-sm md:max-w-3xl py-8">
+          <Signup />
+        </div>
       </div>
+      {/* <div>
+        <Signup />
+      </div> */}
     </>
   );
 };
