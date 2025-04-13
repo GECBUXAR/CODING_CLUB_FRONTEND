f2a8f3dcce24a95a9241fff8ed0a0bdf1134f3a6
Azlan Ehtasham <azlan.ehtasham@gmail.com>
2025-04-13 22:14:07 +0530

    Create simplified routes and app to fix blank screen issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,15 @@
 import React from "react";
-import { AuthProvider } from "./contexts/optimized-auth-context";
-import { NotificationProvider } from "./contexts/optimized-notification-context";
-import { ResponseEvaluationProvider } from "./contexts/optimized-response-evaluation-context";
-import { ExamProvider } from "./contexts/optimized-exam-context";
-import { EventProvider } from "./contexts/optimized-event-context";
-import { FacultyProvider } from "./components/faculty/OptimizedFacultyContext";
-import { RateLimitProvider } from "./contexts/rate-limit-context";
-import NotificationCenter from "./components/common/notification-center";
-import ApiConnectionWarning from "./components/common/cors-warning";
-import AppInitializer from "./components/common/AppInitializer";
-import AppRoutes from "./routes";
-import { Toaster } from "sonner";
+import AppRoutes from "./simple-routes";
 
+// Temporarily simplified App component to diagnose blank screen issue
 const App = () => {
   return (
-    <AuthProvider>
-      <NotificationProvider>
-        <RateLimitProvider>
-          <NotificationCenter />
-          <ResponseEvaluationProvider>
-            <ExamProvider>
-              <EventProvider>
-                <FacultyProvider>
-                  {/* Add AppInitializer to handle controlled data loading */}
-                  <AppInitializer />
-                  <AppRoutes />
-                  <ApiConnectionWarning />
-                  <Toaster position="top-right" richColors closeButton />
-                </FacultyProvider>
-              </EventProvider>
-            </ExamProvider>
-          </ResponseEvaluationProvider>
-        </RateLimitProvider>
-      </NotificationProvider>
-    </AuthProvider>
+    <div className="app-container">
+      <h1>Coding Club App</h1>
+      <AuthProvider>
+        <AppRoutes />
+      </AuthProvider>
+    </div>
   );
 };
 
