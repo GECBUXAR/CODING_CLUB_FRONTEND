01a58fbd3046e613782a900fa0d15c3d057160b2
Azlan Ehtasham <azlan.ehtasham@gmail.com>
2025-04-13 22:14:07 +0530

    Create simplified routes and app to fix blank screen issue

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,12 @@
-import { StrictMode } from "react";
+import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
 
+// Temporarily remove StrictMode to help diagnose issues
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
