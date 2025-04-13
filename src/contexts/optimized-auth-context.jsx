58e7ccf43afdd997f4cd88b06c7d3f16b95c8436
Azlan Ehtasham <azlan.ehtasham@gmail.com>
2025-04-13 23:36:02 +0530

    Add missing useRef import to optimized-auth-context.jsx

--- src/contexts/optimized-auth-context.jsx
+++ src/contexts/optimized-auth-context.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useMemo } from "react";
+import React, { useEffect, useState, useMemo, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import * as authService from "../services/authService";
 import enhancedApiClient from "../services/enhancedApi";
