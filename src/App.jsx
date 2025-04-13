1ef46b63485bae1829c1f2d4c4a415aa6ad60afb
Azlan Ehtasham <azlan.ehtasham@gmail.com>
2025-04-13 22:46:53 +0530

    Restore full application functionality with fixed UI components

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,21 @@
 import React from "react";
 import { AuthProvider } from "./contexts/optimized-auth-context";
 import { NotificationProvider } from "./contexts/optimized-notification-context";
-import { ResponseEvaluationProvider } from "./contexts/optimized-response-evaluation-context";
-import { ExamProvider } from "./contexts/optimized-exam-context";
-import { EventProvider } from "./contexts/optimized-event-context";
-import { FacultyProvider } from "./components/faculty/OptimizedFacultyContext";
 import { RateLimitProvider } from "./contexts/rate-limit-context";
 import NotificationCenter from "./components/common/notification-center";
 import ApiConnectionWarning from "./components/common/cors-warning";
-import AppInitializer from "./components/common/AppInitializer";
 import AppRoutes from "./routes";
 import { Toaster } from "sonner";
+
 const App = () => {
   return (
     <AuthProvider>
       <NotificationProvider>
         <RateLimitProvider>
           <NotificationCenter />
-          <ResponseEvaluationProvider>
-            <ExamProvider>
-              <EventProvider>
-                <FacultyProvider>
-                  {/* Add AppInitializer to handle controlled data loading */}
-                  <AppInitializer />
-                  <AppRoutes />
-                  <ApiConnectionWarning />
-                  <Toaster position="top-right" richColors closeButton />
-                </FacultyProvider>
-              </EventProvider>
-            </ExamProvider>
-          </ResponseEvaluationProvider>
+          <AppRoutes />
+          <ApiConnectionWarning />
+          <Toaster position="top-right" richColors closeButton />
         </RateLimitProvider>
       </NotificationProvider>
     </AuthProvider>
